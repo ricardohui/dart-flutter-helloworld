a5df96e1b68d7898190074d70925c65164ef9393
Ricardo <eehaha@gmail.com>
2019-09-26 08:43:26 +0800

    take user input and add to transaction list

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,45 @@
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
+import './widgets/user_transactions.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-  var totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      questionIndex = 0;
-      totalScore = 0;
-    });
-  }
-
-  final questions = const [
-    {
-      "questionText": "What is your favorite color?",
-      "answers": [
-        {'text': "Black", "score": 2},
-        {"text": "Red", "score": 1},
-        {"text": "Green", "score": 3},
-        {"text": "White", "score": 4}
-      ],
-    },
-    {
-      "questionText": "What is your favorite animal?",
-      "answers": [
-        {"score": 4, "text": "Rabbit"},
-        {"score": 3, "text": "Snake"},
-        {"score": 2, "text": "Elephant"},
-        {"text": "Lion", "score": 1}
-      ],
-    },
-    {
-      "questionText": "Who is your favorite instructor?",
-      "answers": [
-        {"text": "Ricardo", "score": 4},
-        {"text": "Stephen", "score": 3},
-        {"text": "Max", "score": 2},
-        {"text": "Jacky", "score": 1}
-      ],
-    }
-  ];
-  void answerQuestion(int score) {
-    totalScore += score;
-    if (questionIndex < questions.length) {}
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-
-    print(questionIndex);
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First App'),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
-            body: questionIndex < questions.length
-                ? Quiz(
-                    answerQuestion: answerQuestion,
-                    questions: questions,
-                    questionIndex: questionIndex,
-                  )
-                : Result(totalScore, _resetQuiz)));
+          ),
+          UserTransactions()
+        ],
+      ),
+    );
   }
 }
