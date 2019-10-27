e1ce4d034fb31e566a2bb240f1808e07684dbb6b
Ricardo Hui <eehaha@gmail.com>
2019-10-28 07:51:07 +0800

    set up the Great Place Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/great_places.dart';
+import './screens/places_list_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+          title: 'Great Places',
+          theme: ThemeData(
+            primarySwatch: Colors.indigo,
+            accentColor: Colors.amber,
           ),
-          body: Container(
-            child: Text('somethiing'),
-          )),
+          home: PlacesListScreen()),
     );
   }
 }
