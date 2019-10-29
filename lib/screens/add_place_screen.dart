825758c06e6c028e5e1d0cf9ec59ff67eee5ebbc
Ricardo Hui <eehaha@gmail.com>
2019-10-30 06:34:04 +0800

    Revert "unknown commit by system"
    This reverts commit 084ea181e46c9eec6707244b7c34bf659572019e.

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
-import '../widgets/location_input.dart';
 import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -54,8 +53,6 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       height: 10,
                     ),
                     ImageInput(_selectImage),
-                    SizedBox(height: 10,),
-                    LocationInput(),
                   ],
                 ),
               ),
