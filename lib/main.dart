ad0a88eedea1bba3a309a0cde8cfa90d0cb3520d
Ricardo Hui <eehaha@gmail.com>
2019-11-04 11:39:24 +0800

    play around row and column

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Widget buildColorBox(Color color) {
+    return Expanded(
+        child: Container(
+      margin: EdgeInsets.all(20),
+      padding: EdgeInsets.all(30),
+      color: color,
+    ));
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('My First App'),
+          title: Text(
+            'My First App',
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
           ),
           body: Container(
-            child: Text('somethiing'),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          textDirection: TextDirection.ltr,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                buildColorBox(Colors.grey),
+                buildColorBox(Colors.red),
+                buildColorBox(Colors.cyan),
+                buildColorBox(Colors.yellow),
+              ],
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                // verticalDirection: VerticalDirection.up,
+                textDirection: TextDirection.rtl,
+                children: [
+                  buildColorBox(Colors.grey),
+                  buildColorBox(Colors.red),
+                  buildColorBox(Colors.cyan),
+                  buildColorBox(Colors.yellow),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.green[500]),
+                Icon(Icons.star, color: Colors.black),
+                Icon(Icons.star, color: Colors.black),
+              ],
+            ),
+          ],
           )),
+      ),
     );
   }
 }
