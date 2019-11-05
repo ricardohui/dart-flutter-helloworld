78007f17a9a7d230d8e6a88765fa54bccb6cd31c
Ricardo Hui <eehaha@gmail.com>
2019-11-06 07:05:27 +0800

    add Card and ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,39 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Widget _buildCard() => SizedBox(
+        height: 210,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('1625 Main Street',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              subtitle: Text('My City, CA 99984'),
+              leading: Icon(
+                Icons.restaurant_menu,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('(408) 555-1212',
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              leading: Icon(
+                Icons.contact_phone,
+                color: Colors.blue[500],
+              ),
+            ),
+            ListTile(
+              title: Text('costa@example.com'),
+              leading: Icon(
+                Icons.contact_mail,
+                color: Colors.blue[500],
+              ),
+            ),
+          ],
+        ),
+      );
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
@@ -17,7 +50,7 @@ class _MyAppState extends State<MyApp> {
             title: Text('My First App'),
           ),
           body: Container(
-            child: Text('somethiing'),
+            child: _buildCard(),
           )),
     );
   }
