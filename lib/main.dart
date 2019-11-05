94717962bbf70757f43c32efc94eac051f241ebd
Ricardo Hui <eehaha@gmail.com>
2019-11-06 07:27:58 +0800

    adding the title section

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,38 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Widget _titleSection = Container(
+      padding: EdgeInsets.all(32.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                Text('Kandersteg, Switzerland',
+                    style: TextStyle(color: Colors.grey))
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text('41')
+        ],
+      ));
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter layout demo',
       home: Scaffold(
           appBar: AppBar(
-            title: Text('My First App'),
+            title: Text('Flutter layout demo'),
           ),
-          body: Container(
-            child: Text('somethiing'),
+          body: Column(
+            children: <Widget>[_titleSection],
           )),
     );
   }
