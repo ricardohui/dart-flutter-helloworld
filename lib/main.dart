a910be5e3fc81cc588129a8d1e5676440f59b154
Ricardo Hui <eehaha@gmail.com>
2019-11-10 15:53:31 +0800

    4 rows of texts and the display bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +11,77 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var displayText = '0';
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Container(
-            child: Text('somethiing'),
-          )),
+        appBar: AppBar(
+          title: Text('My First Calculator'),
+        ),
+        body: Container(
+            child: Column(
+          children: <Widget>[
+            Text(
+              displayText,
+              style: TextStyle(fontSize: 72),
+              textAlign: TextAlign.right,
+            ),
+            Container(
+                child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Keypad(
+                      displayText: '9',
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Keypad(
+                      displayText: '6',
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Keypad(
+                      displayText: '3',
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Keypad(
+                      displayText: 'C',
+                    ),
+                  ],
+                ),
+              ],
+            ))
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class Keypad extends StatelessWidget {
+  final String displayText;
+  Keypad({@required this.displayText});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      child: Center(
+          child: Text(
+        displayText,
+        style: TextStyle(fontSize: 36),
+      )),
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(32),
+      color: Colors.blue,
     );
   }
 }
