da9675303496091bd4c554355a987b5ea0df90a7
Ricardo Hui <eehaha@gmail.com>
2019-11-12 21:09:01 +0800

    add the layout before adding provider

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Container(
-            child: Text('somethiing'),
-          )),
+      title: 'Provider Demo',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: new MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('pressed'),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text('My First App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many time:'),
+            Text('0'),
+          ],
+        ),
+      ),
     );
   }
 }
