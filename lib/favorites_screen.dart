d3f78d1fb5a8e6557c8fffa3a725d5e9db81f9e5
Ricardo Hui <eehaha@gmail.com>
2019-11-12 08:41:02 +0800

    Scaffold is not need inside screen under tab

--- lib/favorites_screen.dart
+++ lib/favorites_screen.dart
@@ -3,13 +3,8 @@ import 'package:flutter/material.dart';
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Favorites'),
-      ),
-      body: Center(
-        child: Text('This is Favorites'),
-      ),
+    return Center(
+      child: Text('This is Favorites'),
     );
   }
 }
