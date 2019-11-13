0973b886be9c435b44847a6a718cda348a12c4d4
Ricardo Hui <eehaha@gmail.com>
2019-11-13 11:51:22 +0800

    add api call and data fetching

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './models/posts.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
+}
+
+Future<Post> fetchPost() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/posts/1');
+  if (response.statusCode == 200) {
+    return Post.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load post');
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -10,15 +23,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Future<Post> post;
+
+  @override
+  initState() {
+    super.initState();
+    post = fetchPost();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Container(
-            child: Text('somethiing'),
-          )),
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Post>(
+              future: post,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.title);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+                return CircularProgressIndicator();
+              }),
+        ),
+      ),
     );
   }
 }
