fc061c76c832e9620ac90d453c463a29dac51bd5
Ricardo Hui <eehaha@gmail.com>
2019-11-14 22:39:22 +0800

    complete part 2 of the tutorial
    add a screen on the fly for favourite startup name suggestion.

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,44 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
-    return MaterialApp(home: RandomWords(), title: 'Startup Name Generator');
+    return MaterialApp(
+      home: RandomWords(),
+      theme: ThemeData(primaryColor: Colors.white),
+      title: 'Startup Name Generator',
+    );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          );
+        });
+        final List<Widget> divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestion'),
+            ),
+            body: ListView(
+              children: divided,
+            ));
+      }),
+    );
+  }
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -36,11 +67,23 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
@@ -49,6 +92,12 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved,
+          )
+        ],
       ),
       body: _buildSuggestions(),
     );
