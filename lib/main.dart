d4553d3c53fe5ab6c797c6672240e641bb0854b7
Ricardo Hui <eehaha@gmail.com>
2019-11-14 16:40:08 +0800

    A basic styled  TextFormField without validation

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,37 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Title'),
           ),
-          body: Container(
-            child: Text('somethiing'),
-          )),
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+          ),
+        ],
+      ),
     );
   }
 }
